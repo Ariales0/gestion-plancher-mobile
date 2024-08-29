8a08a11319cea780d68f096ff73b8a5a9be61568
Steve <2265857@cegeprdl.ca>
2024-08-29 14:22:27 -0400

    Création page1-2 exercice#1

---
+++ pages/Page1.tsx
@@ -0,0 +1,13 @@
+import { Button, View, Text } from 'react-native'
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <View>
+      <Text>J'ai fait ma première app react native !</Text>
+      <Button title="Viens voir ce que j'ai réalisé!" />
+    </View>
+  )
+}
+
+export default Page1
---
+++ pages/Page2.tsx
@@ -0,0 +1,13 @@
+import { Button, View, Text } from 'react-native'
+import React from 'react'
+
+const Page2 = () => {
+  return (
+    <View>
+      <Text>Je deviens un pro =D </Text>
+      <Button title="Précédent" />
+    </View>
+  )
+}
+
+export default Page2
