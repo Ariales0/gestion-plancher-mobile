2b29c49be1fca104da0c6224768a495baa652e6c
Steve <2265857@cegeprdl.ca>
2024-09-02 10:05:39 -0400

    Page Login Visuel complété

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,39 @@
-import { Button, View, Text , StyleSheet, TouchableOpacity} from 'react-native'
+import { Button, View, Text , TextInput, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Login = ({ navigation }) => {
   return (
-    <View style={styles.container} >
-      <View style={styles.horizontalContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Thermostats')}>
-        <Text style={styles.item}>Thermostats</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Réglages')}>
-        <Text style={styles.item}>Réglages</Text>
+    <View style={styles.container}>
+      {/* Banner */}
+      <View style={styles.banner}>
+        <Image source={require('../assets/images/Banner.png')} 
+        style={styles.bannerImage}
+        resizeMode='contain'/>
+      </View>
+      <View style={styles.loginContainer}>
+        <Text style={{fontSize: 16, fontWeight: 'bold', marginBottom: 4}}>Veuillez vous connecter avec votre compte.</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Saissisez votre nom d'utilisateur" />
+          <TextInput style={styles.input} placeholder="Saissisez votre mot de passe"
+          secureTextEntry/>
+        </View>
+        {/* <button title="Identifiant" onPress={() => navigation.navigate('Thermostats')} />
+         */}
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => navigation.navigate('Thermostats')}>
+          <Text style={styles.buttonText}>Identifiant</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.horizontalContainer}>
-      
-      </View>
       {/* Footer */}
         <View style={styles.footer}>
           <View style={styles.topbar}/>
         <Text style={styles.footerText} >Version: 1.01</Text>
       </View>
-    </View>
+      </View>
   )
 }
 
@@ -30,9 +41,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingLeft: 20,
+    // padding: 5,
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    alignItems: 'center',
+  },
+  banner: {
+    width: '100%', 
+    height: Dimensions.get('window').height *0.3,
+    position: 'absolute',
+    top: 0,
+    marginTop: -55,
+  },
+  bannerImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover', // Ajuste l'image pour couvrir toute la bannière
+  },
+  loginContainer: {
+    height: 800,
+    width: '75%',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer:{
+  },
+  input: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#e2e2e2',
+    padding: 5,
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#F78D1F',
+    color: '#FFF',
+    padding: 10,
+    borderRadius: 5,
+    width: '50%',
+    marginTop: 30
+  },
+  buttonText: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 18,
   },
   horizontalContainer: {
     flexDirection: 'row', 
@@ -56,7 +108,7 @@ const styles = StyleSheet.create({
 
   },
   topbar: {
-    height: 5,
+    height: 4,
     backgroundColor: '#F78D1F',
     width: '120%',
     marginTop: -10,
